f72210d9b1ca1f43a8b135870b4a1f8b32db6734
Tim Buchner <twometer@outlook.de>
2021-01-17 16:47:05 +0100

    v0.2.0: Can now write whole directories

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <getopt.h>
 #include <fstream>
 #include <cmath>
+#include <filesystem>
 
 #include "file_content.h"
 #include "buffer.h"
@@ -9,7 +10,7 @@
 using namespace std;
 
 void bad_args(char **argv) {
-    std::cerr << "Usage: " << argv[0] << " -s <bootSectorPath> -l <bootLoaderPath> -k <kernelPath> -o <output>"
+    std::cerr << "Usage: " << argv[0] << " -s <bootSectorPath> -l <bootLoaderPath> -r <fileSystemRoot> -o <output>"
               << std::endl;
     exit(1);
 }
@@ -41,23 +42,26 @@ void log(const std::string &msg) {
 void
 write_dirent(buffer &buf, int offset, const std::string &name, uint8_t attribute, uint16_t cluster, uint32_t filesize) {
     char eightPoint3[12];
+    memset(eightPoint3, 0, 12);
+
     memcpy(eightPoint3, name.c_str(), name.length());
-    buf.write_at(offset, (const char *)eightPoint3);
+    buf.write_at(offset, (const char *) eightPoint3);
     buf.write_at(offset + 11, attribute);
     buf.write_at(offset + 26, cluster);
     buf.write_at(offset + 28, filesize);
 
-    std::cout << eightPoint3 << " as " << (int)attribute << " at " << offset << " (" << filesize << " B)" << std::endl;
+    std::cout << "DIRENT: " << eightPoint3 << " as " << (int) attribute << " at " << offset << " (" << filesize << " B)"
+              << std::endl;
 }
 
 int main(int argc, char *argv[]) {
     std::string bootSectorPath;
     std::string bootLoaderPath;
-    std::string kernelPath;
+    std::string fsRootPath;
     std::string outputPath;
 
     int opt;
-    while ((opt = getopt(argc, argv, "s:l:k:o:")) != -1) {
+    while ((opt = getopt(argc, argv, "s:l:r:o:")) != -1) {
         switch (opt) {
             case 's':
                 bootSectorPath = std::string(optarg);
@@ -65,8 +69,8 @@ int main(int argc, char *argv[]) {
             case 'l':
                 bootLoaderPath = std::string(optarg);
                 break;
-            case 'k':
-                kernelPath = std::string(optarg);
+            case 'r':
+                fsRootPath = std::string(optarg);
                 break;
             case 'o':
                 outputPath = std::string(optarg);
@@ -76,11 +80,11 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    if (bootLoaderPath.empty() || bootLoaderPath.empty() || kernelPath.empty() || outputPath.empty()) {
+    if (bootLoaderPath.empty() || bootLoaderPath.empty() || fsRootPath.empty() || outputPath.empty()) {
         bad_args(argv);
     }
 
-    log("Neko image builder v0.1.0");
+    log("** Neko image builder v0.2.0 **");
 
     const int image_size = 16 * 1024 * 1024;
     const int first_partition_block = 128;
@@ -93,30 +97,29 @@ int main(int argc, char *argv[]) {
     const int blocks_per_fat = 256;
     const int hidden_blocks = 128;
     const int total_sectors = image_size / 512 - first_partition_block;
-    const int kernel_file_cluster = 3;
     const int cluster_size = bytes_per_block * blocks_per_alloc;
 
     buffer buf(image_size);
 
     // Write boot sector
-    log("writing boot sector...");
+    log("WRITE: Boot sector");
     file_content bootSector = read_binary(bootSectorPath);
     buf.write_at(0, bootSector);
 
     // Write boot loader
-    log("writing boot loader...");
+    log("WRITE: Boot loader");
     file_content bootLoader = read_binary(bootLoaderPath);
     buf.write_at(512, bootLoader);
 
     // Master Boot Record
-    log("generating MBR...");
+    log("WRITE: Master Boot Record");
     buf.write_at<uint8_t>(0x1BE + 0x00, 0x80); // Bootable
     buf.write_at<uint8_t>(0x1BE + 0x04, 0x0E); // Disk type
     buf.write_at<int32_t>(0x1BE + 0x08, first_partition_block);
     buf.write_at<int32_t>(0x1BE + 0x0C, total_sectors);
 
     // FAT Partition
-    log("generating FAT partition header");
+    log("WRITE: FAT partition header");
     int first_partition_byte_offset = bytes_per_block * first_partition_block;
     buf.write_at(first_partition_byte_offset + 0x03, "MSWIN4.1");
     buf.write_at<int16_t>(first_partition_byte_offset + 0x0b, bytes_per_block);
@@ -129,44 +132,56 @@ int main(int argc, char *argv[]) {
     buf.write_at<int16_t>(first_partition_byte_offset + 0x1c, hidden_blocks);
     buf.write_at<int16_t>(first_partition_byte_offset + 0x20, total_sectors);
 
-    std::cout << "partition has " << total_sectors << " sectors" << std::endl;
+    std::cout << "  partition has " << total_sectors << " sectors" << std::endl;
 
-    // Kernel
-    log("generating FAT");
-    file_content kernel = read_binary(kernelPath);
+    // Write directory
+    int root_dir_begin = bytes_per_block * (first_partition_block + reserved_blocks + blocks_per_fat * fat_count);
+    std::cout << "ROOTDIR: At " << root_dir_begin << std::endl;
 
-    // FAT table
+    write_dirent(buf, root_dir_begin, "NEKOSYS", 0x08u, 0, 0);
+    int root_dir_end = root_dir_begin + root_dir_entries * 32;
+    int next_root_dir_ent = root_dir_begin + 32;
+
+    std::cout << std::endl;
+
+    // Write data
     int fat0_idx = bytes_per_block * (first_partition_block + reserved_blocks);
     int fat1_idx = bytes_per_block * (first_partition_block + reserved_blocks + blocks_per_fat);
 
-    int total_clusters = ceil((double) kernel.length / (double) cluster_size);
-    std::cout << "kernel uses " << total_clusters << " clusters (" << kernel.length << "; " << cluster_size << ")"
-              << std::endl;
+    int cluster = 3;
 
-    for (int i = 0; i < total_clusters; i++) {
-        int currentClusterIdx = kernel_file_cluster + i;
-        int currentClusterOffset = currentClusterIdx * 2;
-        uint16_t next_cluster = (i < total_clusters - 1) ? (currentClusterIdx + 1) : 0xFFFF;
-        std::cout << currentClusterIdx << " ref " << next_cluster << std::endl;
+    for (const auto &entry : std::filesystem::directory_iterator(fsRootPath)) {
+        auto file = read_binary(entry.path().string());
 
-        buf.write_at(fat0_idx + currentClusterOffset, next_cluster);
-        buf.write_at(fat1_idx + currentClusterOffset, next_cluster);
+        int total_clusters = ceil((double) file.length / (double) cluster_size);
+        std::cout << "FILE: " << entry.path().filename().string() << " (" << total_clusters << " clusters, "
+                  << file.length << " B) at " << cluster << std::endl;
+
+        // write FAT
+        for (int i = 0; i < total_clusters; i++) {
+            int currentClusterIdx = cluster + i;
+            int currentClusterOffset = currentClusterIdx * 2;
+            uint16_t next_cluster = (i < total_clusters - 1) ? (currentClusterIdx + 1) : 0xFFFF;
+            std::cout << "  " << currentClusterIdx << " -> " << next_cluster << std::endl;
+
+            buf.write_at(fat0_idx + currentClusterOffset, next_cluster);
+            buf.write_at(fat1_idx + currentClusterOffset, next_cluster);
+        }
+
+        // write dirent
+        write_dirent(buf, next_root_dir_ent, entry.path().filename().string(), 0, cluster, file.length);
+
+        // write actual file data
+        int file_offset = root_dir_end + cluster_size * (cluster - 2);
+        buf.write_at(file_offset, file);
+
+        next_root_dir_ent += 32;
+        cluster += total_clusters;
+        std::cout << std::endl;
     }
 
-    // Root directory
-    log("generating rootdir");
-    int root_dir_begin = bytes_per_block * (first_partition_block + reserved_blocks + blocks_per_fat * fat_count);
-    std::cout << "root dir at " << root_dir_begin << std::endl;
-    write_dirent(buf, root_dir_begin, "NEKOSYS", 0x08u, 0, 0);
-    write_dirent(buf, root_dir_begin + 32, "NEKOKRNL", 0, kernel_file_cluster, kernel.length);
-    int root_dir_end = root_dir_begin + root_dir_entries * 32;
-
-    int kernel_file_offset = root_dir_end + cluster_size * (kernel_file_cluster - 2);
-    std::cout << "writing kernel data to " << kernel_file_offset << std::endl;
-    buf.write_at(kernel_file_offset, kernel);
-
     // Write output
-    log("writing image file...");
+    log("OUTPUT: Writing image file...");
     write_binary(outputPath, buf.to_file_content());
 
     // We're done
--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./cmake-build-debug/image_builder -s ../nekosys/build/boot.bin -l ../nekosys/build/loader.bin -k ../nekosys/build/NEKOKRNL -o ../nekosys/build/neko.img
+./cmake-build-debug/image_builder -s ../nekosys/build/boot.bin -l ../nekosys/build/loader.bin -r ../nekosys/fsroot -o ../nekosys/build/neko.img
